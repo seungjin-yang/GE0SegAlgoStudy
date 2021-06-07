bdb1e67849a9d88062a3ec8caf42238309884f4d
Seungjin Yang <seungjin.yang@cern.ch>
2021-06-07 15:11:25 +0900

    fix SealModule

--- GE0Segment/plugins/SealModule.cc
+++ GE0Segment/plugins/SealModule.cc
@@ -2,7 +2,5 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "MuonTriggering/GE0Segment/plugins/GE0DatasetWriter.h"
-#include "MuonTriggering/GE0Segment/plugins/GE0GeometryDumper.h"
 
 DEFINE_FWK_MODULE(GE0DatasetWriter);
-DEFINE_FWK_MODULE(GE0GeometryDumper);
